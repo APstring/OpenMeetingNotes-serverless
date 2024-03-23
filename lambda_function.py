8910df1695ceb4a54147601246a33a3697fc775e
APstring <anand.parthiban@gmail.com>
2024-03-23 01:04:12 -0700

    added skeleton

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,17 @@
-from openai import OpenAI
 import traceback
 import json
+import boto3
+from openai import OpenAI
+from config import Config
+
+BUCKET_NAME = "openmeeting-serverless-mp3"
+
 
 # SNS, SQS for data in transit, S3, Aurora, DynamoDB for persistence
-def speech_to_text(client, audio_path):
+def speech_to_text(client, config, file_key):
 
-
-    audio_file= open(audio_path, "rb")
+    response = client.get_object(Bucket=BUCKET_NAME, Key=file_key)
+    audio_file = response['Body'].read()
 
     try:
         transcription = client.audio.transcriptions.create(
@@ -42,13 +47,14 @@ def text_to_summary(client, content, summary_size=150):
     
     return response
 
+## uploads mp3 to bucket and returns object name ##
+## is this a frontend process? ##
+def upload_audio_file(file, config):
+    bucket = boto3.client("s3")
 
-def upload_audio_file():
-    return
+    response = bucket.put_object(Bucket=config["BUCKET_NAME"], Key=config["BUCKET_KEY"], Body=file)
+    return response
 
-
-def retrieve_audio_path():
-    return
 
 
 ## if uploading mp3, then activating summarizer, 
@@ -61,22 +67,30 @@ def lambda_handler(event, context):
     print(f"request event: {event}")
     client = OpenAI()
 
-    if :
-        id = upload_audio_file(path)
-        return {"statusCode": 200, "body": {"audio_path": path}}
-    else:
-        
+    config = Config()
 
-
+    
     if 'body'in event and 'id' in event['body']['id']:
-        audio_id = event['body']['id']
+        audio_file_key = event['body']['key']
     else:
         print("Invalid input: Incorrect format.")
         return {"statusCode": 401, "body": "Invalid input: Incorrect format."}
     
-    ## endpoint for uploading mp3 ##
+    ## endpoint for processing speech to text ##
+    response = speech_to_text(client, config, audio_file_key)
+
+    if not response:
+
+    if 'status' == 
 
     ## endpoint for summarizing ##
+    text_to_summary(client, content=)
 
 
     return resp
+
+
+if __name__ == "__main__":
+    id = upload_audio_file(path)
+    # return {"statusCode": 200, "body": {"audio_path": path}}
+    
